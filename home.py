456b50fe59487fa6eb94b11916c2840291334fcd
Sinan Vanlı <sinan.vanli@gmail.com>
2018-01-28 12:51:58 +0000

    Introduced logging

--- home.py
+++ home.py
@@ -5,6 +5,7 @@ import RPi.GPIO as GPIO
 import os
 import glob
 import time
+import logging
 
 # Made by:
 # Burak Vanli
@@ -30,34 +31,36 @@ GPIO.setup(33, GPIO.OUT)
 time1= ''
 win = Tk()
 
+logging.basicConfig(filename = 'log.log', filemode = 'w', level = logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
 bf=open ('boilerfile.txt', 'r')
 bf.seek(-1, 2)
 boilerState=bf.read(1)
-print('Boiler is ', boilerState)
+logging.info('Boiler is', boilerState)
 bf.close()
 
 wf=open ('waterfile.txt', 'r')
 wf.seek(-1, 2)
 waterState=wf.read(1)
-print('Watering is ', waterState)
+logging.info('Watering is ', waterState)
 wf.close()
 
 lf=open ('lampfile.txt', 'r')
 lf.seek(-1, 2)
 lampState = lf.read(1)
-print('Lamp is ', lampState)
+logging.info('Lamp is ', lampState)
 lf.close()
 
 yf=open ('SICAKSUfile.txt', 'r')
 yf.seek(-1, 2)
 SICAKSUState=yf.read(1)
-print('SICAKSU is ', SICAKSUState)
+logging.info('SICAKSU is ', SICAKSUState)
 yf.close()
 
 hf=open ('hidrofile.txt', 'r')
 hf.seek(-1, 2)
 hidroState=hf.read(1)
-print('HIDROFOR is ', hidroState)
+logging.info('HIDROFOR is ', hidroState)
 hf.close()
 
 
@@ -67,82 +70,78 @@ clock = Label(win, font = myFont, height = 2, width = 20)
 clock.grid(row = 1, column = 1)
 
 def read_temp_raw():
-        f = open(device_file, 'r')
-        lines = f.readlines()
-        f.close()
-        return lines
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
 
 def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
         lines = read_temp_raw()
-        while lines[0].strip()[-3:] != 'YES':
-                time.sleep(0.2)
-                lines = read_temp_raw()
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
-                temp_string = lines[1][equals_pos+2:]
-                temp_c = float(temp_string) / 1000.0
-                return temp_c
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            return temp_c
 
 #***************************************
 def boilerON():
-        print("BOILER button pressed")
-        bf=open('boilerfile.txt', 'w')
-        if GPIO.input(12) :
-                GPIO.output(12,GPIO.LOW)
-                GPIO.output(29,GPIO.LOW)
-                GPIO.output(31,GPIO.LOW)
-		bf.write('1')
-		boilerButton["text"] = "ISITMA ON"
-		print time1, 'BOILER ON'
-		bf.close()
-	else:
-		GPIO.output(12, GPIO.HIGH)
+    logging.info("BOILER button pressed")
+    bf=open('boilerfile.txt', 'w')
+    if GPIO.input(12) :
+        GPIO.output(12,GPIO.LOW)
+        GPIO.output(29,GPIO.LOW)
+        GPIO.output(31,GPIO.LOW)
+        bf.write('1')
+        boilerButton["text"] = "ISITMA ON"
+        logging.info(time1, 'BOILER ON')
+        bf.close()
+    else:
+        GPIO.output(12, GPIO.HIGH)
 		GPIO.output(29,GPIO.HIGH)
-                GPIO.output(31,GPIO.HIGH)
-		bf.write('0')
-		boilerButton["text"] = "ISITMA OFF"
-		print time1, 'BOILER OFF'
-		bf.close()
-
+        GPIO.output(31,GPIO.HIGH)
+        bf.write('0')
+        boilerButton["text"] = "ISITMA OFF"
+        logging.info(time1, 'BOILER OFF')
+        bf.close()
 
 def rainbirdON():
-	print("RAINBIRD button pressed")
-	wf=open ('waterfile.txt', 'w')
+    logging.info("RAINBIRD button pressed")
+    wf=open ('waterfile.txt', 'w')
 	if GPIO.input(11) :
  		GPIO.output(11,GPIO.LOW)
 		wf.write('1')
 		waterButton["text"] = "SULAMA ON"
-		print time1, 'WATER ON'
-                wf.close()
+        logging.info(time1, 'WATER ON')
+        wf.close()
 	else:
 		GPIO.output(11, GPIO.HIGH)
 		wf.write('0')
 		waterButton["text"] = "SULAMA OFF"
-		print time1, 'WATER OFF'
+        logging.info(time1, 'WATER OFF')
 		wf.close()
 
 def lampON():
-	print("LAMP button pressed")
-	print int(int(time.strftime('%M'))%10)%2
+    logging.info("LAMP button pressed")
+    logging.info(int(int(time.strftime('%M'))%10)%2)
 	lf=open ('lampfile.txt', 'w')
 	if GPIO.input(13) :
  		GPIO.output(13,GPIO.LOW)
 		lf.write('1')
 		lampButton["text"] = "LAMBA ON"
-		print time1, 'LAMP ON'
+        logging.info(time1, 'LAMP ON')
 		lf.close()
 	else:
 		GPIO.output(13, GPIO.HIGH)
-		lf.write('0')
+		lf.write('0'
 		lampButton["text"] = "LAMBA OFF"
-		print time1, 'LAMP OFF'
+        logging.info(time1, 'LAMP OFF')
 		lf.close()
 
-
-
-
 def SICAKSUON():
-	print("SICAKSU button pressed")
+    logging.info("SICAKSU button pressed")
 	yf=open ('SICAKSUfile.txt', 'w')
 	#testLabel['text']= time.strftime('%S')
 
@@ -150,106 +149,80 @@ def SICAKSUON():
  		GPIO.output(15,GPIO.LOW)
 		yf.write('1')
 		SICAKSUButton["text"] = "SICAK SU ON"
-		print time1, 'SICAKSU ON'
-                yf.close()
+        logging.info(time1, 'SICAKSU ON')
+        yf.close()
 	else:
 		GPIO.output(15, GPIO.HIGH)
 		yf.write('0')
 		SICAKSUButton["text"] = "SICAK SU OFF"
-		print time1, 'SICAKSU OFF'
+        logging.info(time1, 'SICAKSU OFF')
 		yf.close()
 
-
-
-
 def hidroforON():
-	print("HIDROFOR button pressed")
+    logging.info("HIDROFOR button pressed")
 	hf=open ('hidrofile.txt', 'w')
 	if GPIO.input(33) :
  		GPIO.output(33,GPIO.LOW)
 		hf.write('1')
 		hidroButton["text"] = "HIDRO ON"
-		print time1, 'HIDROFOR ON'
-                hf.close()
+        logging.info(time1, 'HIDROFOR ON')
+        hf.close()
 	else:
 		GPIO.output(33, GPIO.HIGH)
 		hf.write('0')
 		hidroButton["text"] = "HIDRO OFF"
-		print time1, 'HIDROFOR OFF'
+        logging.info(time1, 'HIDROFOR OFF')
 		hf.close()
 
-
-
-
 def exitProgram():
-	print("Exit Button pressed")
+    logging.info("Exit Button pressed")
 	GPIO.cleanup()
 	win.quit()
 
 def lampturnON():
-
 	if GPIO.input(13)==1:#TURN ON
 		lampON()
 
 def lampturnOFF():
-
 	if GPIO.input(13)==0:#TURN OFF
 		lampON()
-
 
 def tick():
 	global time1
 	time2 = time.strftime('%d:%m:%Y - %H:%M:%S')
 	if time2 != time1:
-		time1 = time2
-		clock.config(text=time2)
+        time1 = time2
+        clock.config(text=time2)
 
-                if int(time.strftime('%H')) < 6 and int(time.strftime('%S')) == 0 :
-
-			if int(int(time.strftime('%M'))/15)%2 == 1:
-                		lampturnOFF()
-			else:
-				lampturnON()
-
+        if int(time.strftime('%H')) < 6 and int(time.strftime('%S')) == 0 :
+            if int(int(time.strftime('%M'))/15)%2 == 1:
+                lampturnOFF()
+            else:
+                lampturnON()
 
 		if int(time.strftime('%H')) == 16 and int(time.strftime('%M')) < 1 and int(time.strftime('%S')) == 1:
-			lampturnON()
+            lampturnON()
 
+        #4 YOLLU VANA SIKISMASINI ONLEYICI EGZERSIZ HAREKETI
+        if GPIO.input(12) == 1:
+            # Isitma kapaliysa
+            if int(time.strftime('%H')) % 2 == 0 and GPIO.input(32) == 1:
+                GPIO.output(32, GPIO.LOW)
+                logging.info 'VANA AC'
+                # Vanayi ac
 
-
-                #4 YOLLU VANA SIKISMASINI ONLEYICI EGZERSIZ HAREKETI
-		if GPIO.input(12) == 1:
-                        # Isitma kapaliysa
-                        if int(time.strftime('%H')) % 2 == 0 and GPIO.input(32) == 1:
-                                GPIO.output(32, GPIO.LOW)
-                                print 'VANA AC'
-                                # Vanayi ac
-
-                        if int(time.strftime('%H')) % 2 == 1 and GPIO.input(32) == 0:
-                                GPIO.output(32, GPIO.HIGH)
-                                print 'VANA KAPAT'
-                                # Vanayi kapat
-                        
-                #4 YOLLU VANA EGZERSIZ HAREKETI
-
-
-
-
-
-
-
+                if int(time.strftime('%H')) % 2 == 1 and GPIO.input(32) == 0:
+                    GPIO.output(32, GPIO.HIGH)
+                    logging.info 'VANA KAPAT'
+                    # Vanayi kapat
+        #4 YOLLU VANA EGZERSIZ HAREKETI
 
 		#if int(time.strftime('%H')) == 0 and int(time.strftime('%M')) < 1:
 		#	lampturnOFF()
 
+        clock.after(200, tick)
 
-
-
-
-
-
-
-	clock.after(200, tick)
+# Window settings
 win.title("Yazarlar 22")
 win.geometry('520x600')
 
